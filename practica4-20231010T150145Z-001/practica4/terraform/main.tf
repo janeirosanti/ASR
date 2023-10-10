fe7e62fc814611185ccddd5419fbe18c5bf880ff
Santi <47990780+janeirosanti@users.noreply.github.com>
2023-10-10 17:12:02 +0200

    Add files via upload

---
+++ practica4-20231010T150145Z-001/practica4/terraform/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  project = "practica4-400806"
+  region  = "europe-west1"
+  zone    = "europe-west1-d"
+}
+
+resource "google_compute_instance" "terraform" {
+  name         = "terraform"
+  machine_type = "n1-standard-1"
+  boot_disk {
+    initialize_params {
+      image = "projects/centos-cloud/global/images/centos-7-v20220822"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh-terraform"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "allow-http-terraform"
+  network = "default"
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
---
+++ practica4-20231010T150145Z-001/practica4/terraform10/variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {
+  description = "ID del proyecto de Google Cloud"
+  type        = string
+  default     = "practica4-400806" # Valor predeterminado
+}
